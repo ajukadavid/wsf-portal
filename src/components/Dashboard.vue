a9732f7e3ab117d36add68e435570fb5ea5ec4fe
ithiel883 <ajuka.zephiniah@gmail.com>
2022-09-18 20:02:10 +0100

    added search bar and logo

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,10 +5,51 @@ import { ref } from 'vue'
 </script>
 
 <template>
-  <div class="justify-center flex h-screen">
-    <div class="text-4xl bg-gradient-to-b from-gray-200 via-gray-400 to-gray-600 h-3/4 w-screen">
-      <img src="../assets/wsf-logo.png" alt="winners logo" class="h-20 w-20 ml-5 mt-3"/>
+  <div class="h-screen bg-gradient-to-b from-gray-200 via-gray-400 to-gray-600">
+    <img src="../assets/wsf-logo.png" alt="winners logo" class="h-20 w-20 ml-5 p-3" />
+    <div class="text-4xl  justify-center flex items-center  h-3/4 w-screen">
+      <div class="w-2/4">
+        <form class="flex items-center">
+          <label for="simple-search" class="sr-only">Search</label>
+          <div class="relative w-full">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </div>
+            <input type="text" id="simple-search"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search" required>
+          </div>
+          <button type="submit"
+            class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+            <span class="sr-only">Search</span>
+          </button>
+        </form>
+        <div>
+      <div>
+        <div
+          class="p-2 mt-10 w-60 bg-white rounded-sm flex border border-gray-200 shadow-md dark:bg-white dark:border-gray-100">
+         
+        </div>
+      </div>
+        </div>
+
+
+      </div>
+     
+
+
     </div>
+
   </div>
 </template>
 
