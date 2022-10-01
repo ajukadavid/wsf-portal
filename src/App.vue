c837e9af8e5bc1ec7ff71eab66a4f9ef80f12e23
ithiel883 <ajuka.zephiniah@gmail.com>
2022-10-01 21:53:16 +0100

    setup minister dashboard page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import SpDashboard from './components/SpDashboard.vue'
+
 </script>
 
 <template>
-  <SpDashboard />
+<RouterView />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory  } from 'vue-router';
 import './style.css'
 import App from './App.vue'
+import SpDashboard from '../src/components/SpDashboard.vue'
+import MinisterDashboard from '../src/components/MinisterDashboard.vue'
 import './styles/app.css'; 
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {path: '/', name: 'SpDashboard', component: SpDashboard},
+        {path: '/minister', name: 'MinisterDashboard', component: MinisterDashboard},
 
     ]
 
