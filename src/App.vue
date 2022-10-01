673ac33e0a77b4745d18e0e4956e6fe835cfacd7
ithiel883 <ajuka.zephiniah@gmail.com>
2022-10-01 21:49:18 +0100

    renamed to spdashboard and setup route for minister dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Dashboard from './components/Dashboard.vue'
+import SpDashboard from './components/SpDashboard.vue'
 </script>
 
 <template>
-  <Dashboard />
+  <SpDashboard />
 </template>
 
 <style scoped>
