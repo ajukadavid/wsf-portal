15e5930769480697e63bd77d8899bf1b99c7ba20
ithiel883 <ajuka.zephiniah@gmail.com>
2022-11-13 15:10:31 +0100

    added more input fields and button to signup form

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,13 +4,25 @@
 
 <template>
     <main class="grid h-screen place-items-center">
-        <form>
-            this is sign up route
+        <form @submit.prevent="">
+            <div class="grid justify-items-center mb-5">
+                <p class="font-extrabold">Sign up</p>
+            </div>
+           <p>Enter your email and password to sign up</p>
+           <div class="w-80">    
+            <input type="text" placeholder="Username" class="mt-6 p-4 w-full shadow-md rounded border-0"/>
+           <input type="email" placeholder="Email" class="mt-6 p-4 w-full shadow-md rounded border-0"/>
+           <input type="password" placeholder="Password" class="mt-6 p-4 w-full shadow-md rounded border-0"/>
+           </div>
+           <button type="submit" class="bg-buttonColor">Submit</button>
         </form>
     </main>
 
 </template>
 
-<style scoped lang="scss">
+<style scoped>
+input::placeholder {
+    color: black
+}
 
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      'buttonColor': '#FA4747'
+    }
   },
   variants: {
     extend: {},
